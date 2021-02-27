7edc2dc6b3de1c6b9e7a52b96a5d8fd56d6da2d4
booty <johnedmundrose@gmail.com>
2021-02-27 15:10:52 -0500

    Works, except for slight newline issue

--- ex_1_21.c
+++ ex_1_21.c
@@ -34,36 +34,66 @@ void entab(char line[], int tab_size) {
 	char c=0;
 	int until_next_tab = TAB_SIZE;
 	int consecutive_spaces = 0;
+	int output_col_is_tabstop = 0;
 
-	printf("original line | %s", line);
-	printf("new ver       | ");
+	// printf("--------------|-----------------------------------------\n");
+	// printf("              |01234567890123456789012345678901234567890\n");
+	// printf("              |        T       T       T       T       T\n");
+	// printf("              |-----------------------------------------\n");
+	// printf("original line | %s", line);
+	// printf("new ver       | ");
+	printf("-----------------------------------------\n");
+	printf("01234567890123456789012345678901234567890\n");
+	printf("        T       T       T       T       T\n");
+	printf("-----------------------------------------\n");
+	printf("%s\n", line);
+
 	while ((c = line[input_col]) != '\0') {
-		if (c == '\t') {
-			// jump to next tab stop
-			// reset space counter
-			putchar(c);
-			consecutive_spaces = 0;
-			output_col = next_tab_column(output_col, TAB_SIZE);
+		/*
+		current char is tab?
+		output column is tabstop?
+		current char is space?
+
+		if the output column is a tab stop, then replace all
+		previous spaces (if any) with a tab, and output the current char
+
+		*/
+
+		output_col_is_tabstop = (output_col % TAB_SIZE == 0);
+
+		if (output_col_is_tabstop) {
+			switch(consecutive_spaces) {
+				case 0:
+					// do nothing?
+					break;
+				case 1:
+					putchar(' ');
+					break;
+				default:
+					// putchar('!');
+					putchar('\t');
+			}
+			consecutive_spaces=0;
+			// output_col++;
 		}
-		else if (output_col % TAB_SIZE == 0) {
-			// this is a tab stop
-			// if space counter > 0, output a tab to replace the
-			// spaces we counted
-			//
-			consecutive_spaces = 0;
-			output_col++;
-		}
-		else if (c == ' ') {
-			consecutive_spaces++;
-			output_col++;
-		}
-		else {
-			// it's just a regular character
-			// reset space counter
-			// output the character
-			putchar(c);
-			consecutive_spaces = 0;
-			output_col++;
+
+		switch(c) {
+			case '\t':
+				output_col = next_tab_column(output_col, tab_size);
+				consecutive_spaces=0;
+				putchar(c);
+				break;
+			case ' ':
+				output_col++;
+				consecutive_spaces++;
+				break;
+			default:
+				output_col++;
+				output_col += consecutive_spaces;
+				for (int i=0; i<consecutive_spaces; i++) { putchar(' '); }
+
+				consecutive_spaces=0;
+				putchar(c);
 		}
 
 		input_col++;
