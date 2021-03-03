5ae1fc5fbdaa2d9917f231a8494ee8d8d5d09f85
booty <johnedmundrose@gmail.com>
2021-03-03 03:08:11 -0500

    Cleanup. Leaves blank lines, which is OK.

--- ex_1_23.c
+++ ex_1_23.c
@@ -25,48 +25,50 @@ int main() {
 	char c;
 
 	while ((c = getchar()) != EOF) {
+		if (!inside_character_constant && !inside_string_literal) {
+			if (!inside_line_comment) {
+				// are we entering a block comment?
+				if ((c == '*') && (last_char == BLOCK_START_CHAR)) {
+					inside_block_comment = TRUE;
+				}
+				// are we leaving a block comment?
+				else if ((c == BLOCK_END_CHAR) && (last_char == '*')) {
+					was_inside_comment = TRUE;
+					inside_block_comment = FALSE;
+				}
+			}
 
-		if (!inside_line_comment && !inside_string_literal && !inside_character_constant) {
-			// are we entering a block comment?
-			if ((c == '*') && (last_char == BLOCK_START_CHAR)) {
-				inside_block_comment = TRUE;
-			}
-			// are we leaving a block comment?
-			else if ((c == BLOCK_END_CHAR) && (last_char == '*')) {
-				was_inside_comment = TRUE;
-				inside_block_comment = FALSE;
-				// putchar('!');
-			}
-		}
-
-		if (!inside_block_comment && !inside_string_literal && !inside_character_constant) {
-			if ((c == '/') && (last_char == '/')) {
-				inside_line_comment = TRUE;
-				was_inside_comment = TRUE;
-			}
-			else if (c == '\n') {
-				inside_line_comment = FALSE;
-				was_inside_comment = FALSE;
+			if (!inside_block_comment) {
+				if ((c == '/') && (last_char == '/')) {
+					inside_line_comment = TRUE;
+					was_inside_comment = TRUE;
+				}
+				else if (c == '\n') {
+					inside_line_comment = FALSE;
+					was_inside_comment = FALSE;
+				}
 			}
 		}
 
 		inside_comment = inside_line_comment || inside_block_comment;
 
-		if (!inside_comment && !inside_character_constant && (c == '"')) {
-			if (inside_string_literal && (last_char != '\\')) {
-				inside_string_literal = FALSE;
+		if (!inside_comment) {
+			if (!inside_character_constant && (c == '"')) {
+				if (inside_string_literal && (last_char != '\\')) {
+					inside_string_literal = FALSE;
+				}
+				else {
+					inside_string_literal = TRUE;
+				}
 			}
-			else {
-				inside_string_literal = TRUE;
+
+			if (!inside_string_literal && (c == '\'')) {
+				inside_character_constant = !inside_character_constant;
 			}
-		}
 
-		if (!inside_comment && !inside_string_literal && (c == '\'')) {
-			inside_character_constant = !inside_character_constant;
-		}
-
-		if (!inside_comment && !was_was_inside_comment && (last_char != BOF)) {
-			putchar(last_char);
+			if (!was_was_inside_comment && (last_char != BOF)) {
+				putchar(last_char);
+			}
 		}
 
 		was_was_inside_comment = was_inside_comment;
